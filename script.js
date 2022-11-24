f6de8a52bb7fc938f54869afb0e50481185465fb
Fausto Oliveira <f4_u@hotmail.dem>
2022-11-24 22:48:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function german() {
+    document.getElementById('headline').innerHTML = 'Hallo';
+    document.getElementById('text1').innerHTML = 'Hallo auf diese tollen Seite';
+
+ }
+
+ function english() {
+    document.getElementById('headline').innerHTML = 'Hello';
+    document.getElementById('text1').innerHTML = 'Welcome on my awesome page!';
+
+ }
+
+ function Portuguese() {
+    document.getElementById('headline').innerHTML = 'olá';
+    document.getElementById('text1').innerHTML = 'Bem-vindo à minha página incrível!';
+ }
